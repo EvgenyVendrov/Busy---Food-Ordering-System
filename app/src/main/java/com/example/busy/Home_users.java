d1afd98f3ad6cd9ea8d10677070fa19202e47bc9
TalNoamX <talnoamb@gmail.com>
2019-12-02 18:37:01 +0200

    .

---
+++ app/src/main/java/com/example/busy/Home_users.java
@@ -0,0 +1,14 @@
+package com.example.busy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_users extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_users);
+    }
+}
