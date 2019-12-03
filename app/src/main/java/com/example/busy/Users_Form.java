0db4db5ea2acd12713579c4738bbfaa958f78c43
amitai <amitaymizrahy@gmail.com>
2019-12-03 14:58:13 +0200

    finihed sign up users

--- app/src/main/java/com/example/busy/Users_Form.java
+++ app/src/main/java/com/example/busy/Users_Form.java
@@ -1,15 +1,15 @@
 package com.example.busy;
 
 public class Users_Form {
-    String FirstName;
-    String LastName;
-    String Email;
-    String Type;
+    public String FirstName;
+    public String LastName;
+    public String Email;
+    public final String Type = "User";
 
-    public Users_Form(String FirstName, String Email, String Type, String LastName){
+    public Users_Form(String FirstName, String Email, String LastName){
         this.Email=Email;
         this.FirstName=FirstName;
         this.LastName = LastName;
-        this.Type=Type;
+
     }
 }
--- app/src/main/java/com/example/busy/users/signUpActivity.java
+++ app/src/main/java/com/example/busy/users/signUpActivity.java
@@ -1,20 +1,29 @@
 package com.example.busy.users;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.busy.R;
+import com.example.busy.Users_Form;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class signUpActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editTextFirstName;
     private EditText editTextLastName;
     private EditText editTextEmail;
     private EditText editTextPass;
-    private Button registerButton;
+    private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -27,13 +36,23 @@ public class signUpActivity extends AppCompatActivity implements View.OnClickLis
         editTextEmail = findViewById(R.id.emailText);
         editTextPass = findViewById(R.id.passwordText);
         findViewById(R.id.signUpButton).setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() != null){
+            //handle the alredy login user
+        }
+    }
+
     private void registerUser(){
-        String FirstName = editTextFirstName.getText().toString().trim();
-        String LastName =  editTextLastName.getText().toString().trim();
-        String Email = editTextEmail.getText().toString().trim();
+        final String FirstName = editTextFirstName.getText().toString().trim();
+        final String LastName =  editTextLastName.getText().toString().trim();
+        final String Email = editTextEmail.getText().toString().trim();
         String password = editTextPass.getText().toString().trim();
-        String typeUser = "User";
+
 
         if(FirstName.isEmpty()){
             editTextFirstName.setError("First Name Requierd");
@@ -60,6 +79,33 @@ public class signUpActivity extends AppCompatActivity implements View.OnClickLis
             editTextPass.requestFocus();
             return;
         }
+
+        mAuth.createUserWithEmailAndPassword(Email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //we will store the additional fileds in firebase database
+                            Users_Form user = new Users_Form(FirstName,Email,LastName);
+                            FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getUid()).setValue(user)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(signUpActivity.this, "logged in", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        //display a faliure messege
+                                        Toast.makeText(signUpActivity.this, "culdnt logged in",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+
+                        else{
+                            Toast.makeText(signUpActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     @Override
