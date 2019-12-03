e466f55e4a3a3ddb9d953f62868d6a8b96d139ae
TalNoamX <talnoamb@gmail.com>
2019-12-03 18:55:28 +0200

    starting sign up for rest

--- app/src/main/java/com/example/busy/restaurant/Restaurant_Form.java
+++ app/src/main/java/com/example/busy/restaurant/Restaurant_Form.java
@@ -3,13 +3,11 @@ package com.example.busy.restaurant;
 public class Restaurant_Form {
     private String restaurant_Name;
     private String email;
-    private String password;
     private final String type = "Restaurant";
 
-    public Restaurant_Form(String restaurant_Name, String email, String password, String address){
+    public Restaurant_Form(String restaurant_Name, String email){
         this.restaurant_Name=restaurant_Name;
         this.email=email;
-        this.password=password;
     }
 
     public void setRestaurant_Name(String restaurant_Name) {
@@ -18,17 +16,11 @@ public class Restaurant_Form {
     public void setEmail(String email) {
         this.email = email;
     }
-    public void setPassword(String password) {
-        this.password = password;
-    }
     public String getRestaurant_Name(){
         return this.restaurant_Name;
     }
     public String getEmail(){
         return this.email;
-    }
-    public String getPassword(){
-        return this.password;
     }
     public String getType(){
         return this.type;
--- app/src/main/java/com/example/busy/restaurant/signUp_rest.java
+++ app/src/main/java/com/example/busy/restaurant/signUp_rest.java
@@ -1,14 +1,22 @@
 package com.example.busy.restaurant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.busy.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class signUp_rest extends AppCompatActivity {
+public class signUp_rest extends AppCompatActivity implements View.OnClickListener  {
     private EditText name;
     private EditText email;
     private EditText password;
@@ -23,7 +31,7 @@ public class signUp_rest extends AppCompatActivity {
         name = findViewById(R.id.name_text);
         email = findViewById(R.id.email_text);
         password = findViewById(R.id.pass_text);
-        findViewById(R.id.SignUp_button).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.SignUp_button).setOnClickListener(this);
         mAuth = FirebaseAuth.getInstance();
 
     }
@@ -35,7 +43,7 @@ public class signUp_rest extends AppCompatActivity {
         }
     }
 
-    private void registerUser() {
+    private void registerRest() {
         final String Name = name.getText().toString().trim();
         final String Email = email.getText().toString().trim();
         String Password = password.getText().toString().trim();
@@ -56,6 +64,50 @@ public class signUp_rest extends AppCompatActivity {
             password.setError("Password Required");
             password.requestFocus();
             return;
+        }
+
+        if (Password.length()<6){
+            password.setError("password length shuld be higer than 6");
+            password.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(Email,Password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Restaurant_Form rest = new Restaurant_Form(Name,Email);
+                            FirebaseDatabase.getInstance().getReference("Restaurant").child(FirebaseAuth.getInstance().getUid()).setValue(rest)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(signUp_rest.this,"Sign Up successfully",Toast.LENGTH_SHORT).show();
+                                        Intent i = new Intent (signUp_rest.this,HOME_restaurant.class);
+                                        startActivity(i);
+                                    }
+
+                                    else {
+                                        Toast.makeText(signUp_rest.this,"Error, couldn't sign up",Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                        }
+
+                        else {
+                            Toast.makeText(signUp_rest.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.SignUp_button:
+                registerRest();
+                break;
         }
     }
 }
