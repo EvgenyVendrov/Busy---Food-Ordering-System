83d75c81f9e07ee6d44be205fd7d27d56def6cf5
TalNoamX <talnoamb@gmail.com>
2019-12-05 15:01:27 +0200

    .

--- app/src/main/java/com/example/busy/restaurant/logIn_rest.java
+++ app/src/main/java/com/example/busy/restaurant/logIn_rest.java
@@ -1,15 +1,24 @@
 package com.example.busy.restaurant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.busy.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class logIn_rest extends AppCompatActivity {
+    private EditText _email;
+    private EditText _password;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,14 @@ public class logIn_rest extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(logIn_rest.this, signUp_rest.class);
                 startActivity(i);
+
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        _email = findViewById(R.id.EmailLog);
+        _password = findViewById(R.id.passwordLog);
+
     }
+
 }
