93d0eea43c8b689d3b55564e241d64cba0ce7558
TalNoamX <talnoamb@gmail.com>
2019-12-09 18:30:48 +0200

    .

--- app/src/main/java/com/example/busy/users/Filter_popup.java
+++ app/src/main/java/com/example/busy/users/Filter_popup.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.busy.R;
 
-public class Filter_popup extends AppCompatActivity {
+public class Filter_popup extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,24 @@ public class Filter_popup extends AppCompatActivity {
         int height = dm.heightPixels;
         getWindow().setLayout((int)(width*.8),(int)(.6 * height));
 
+        //Cities spinner
+        Spinner Cities = findViewById(R.id.Cities_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Cities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Cities.setAdapter(adapter);
+        Cities.setOnItemSelectedListener(this);
 
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(parent.getContext(),"All",Toast.LENGTH_SHORT).show();
     }
 }
