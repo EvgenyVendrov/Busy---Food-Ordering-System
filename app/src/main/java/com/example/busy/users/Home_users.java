aa4bd50d29471c73ecb2e07f30a012b839193f0d
amitai <amitaymizrahy@gmail.com>
2019-12-09 13:02:52 +0200

    added Home_users

--- app/src/main/java/com/example/busy/users/Home_users.java
+++ app/src/main/java/com/example/busy/users/Home_users.java
@@ -1,23 +1,30 @@
 package com.example.busy.users;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Filter;
 
 import com.example.busy.R;
 
 public class Home_users extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_users);
+        Button filter = findViewById(R.id.Filter_Button);
 
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home_users.this, Filter_popup.class));
+            }
+        });
     }
+
 }
