5a4c231ed627d7086aacd48212bca6b25c817d55
TalNoamX <talnoamb@gmail.com>
2019-12-09 13:03:39 +0200

    .

---
+++ app/src/main/java/com/example/busy/users/Filter_popup.java
@@ -0,0 +1,29 @@
+package com.example.busy.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import com.example.busy.R;
+
+public class Filter_popup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_popup);
+
+
+        //make the POP-UP window
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+
+    }
+}
