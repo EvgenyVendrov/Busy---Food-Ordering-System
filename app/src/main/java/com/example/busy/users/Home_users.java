ff4cd7961768756fc57a56a50de7f3ba9561609e
TalNoamX <talnoamb@gmail.com>
2019-12-12 17:47:54 +0200

    .

--- app/src/main/java/com/example/busy/users/Home_users.java
+++ app/src/main/java/com/example/busy/users/Home_users.java
@@ -12,31 +12,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.busy.R;
-import com.example.busy.restaurant.forms.Restaurant_Form;
 import com.example.busy.users.Uform.Users_Form;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class Home_users extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
-    Users_Form u;
-    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();//the current online user
-    DatabaseReference ref_users; //the reference for Users realtimedatabase
-    DatabaseReference ref_rests; //the reference for Restaurant realtimedatabase
-    ListView listView;
-    ArrayList<String> rest_list = new ArrayList<>(); //will contains the data of all the restourounts
-    ArrayAdapter<String> rest_adapter; //the addapter that will get the rest_list and will be added to the list view
+    private Users_Form u;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();//the current online user
+    private DatabaseReference ref_users; //the reference for Users realtimedatabase
+    private DatabaseReference ref_rests; //the reference for Restaurant realtimedatabase
+    private ListView listView;
+    private ArrayList<String> rest_list = new ArrayList<>(); //will contains the data of all the restourounts
+    private ArrayAdapter<String> rest_adapter; //the addapter that will get the rest_list and will be added to the list view
+    private String text;
+    private Button searchbtn;
 
 
     @Override
@@ -46,7 +46,7 @@ public class Home_users extends AppCompatActivity implements View.OnClickListene
 
         //Cities Spinner
         Spinner Cities_spinner = findViewById(R.id.Cities_spinner);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.Cities ,android.R.layout.simple_spinner_item);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Cities, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Cities_spinner.setAdapter(adapter);
         Cities_spinner.setOnItemSelectedListener(this);
@@ -74,28 +74,35 @@ public class Home_users extends AppCompatActivity implements View.OnClickListene
         });
 
         //show the testurouns in the database on the page
-        ref_rests.addValueEventListener(new ValueEventListener() {
+       searchbtn = findViewById(R.id.search);
+
+
+    }
+
+    public void getData(View v) {
+
+        Query query = ref_rests.orderByChild("Location").equalTo(text);
+        query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //iterate on the Restourunt nodes
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    String str = ds.getValue().toString();
+                if(dataSnapshot.exists()) {
+                    String str = dataSnapshot.getValue().toString();
                     rest_list.add(str);
+                    rest_adapter = new ArrayAdapter<String>(Home_users.this, android.R.layout.simple_list_item_1, rest_list);
+                    listView.setAdapter(rest_adapter);
                 }
-                //add the array(rest_list) on array addapter and then add the arraydapter to the list view
-                rest_adapter = new ArrayAdapter<String>(Home_users.this, android.R.layout.simple_list_item_1, rest_list);
-                listView.setAdapter(rest_adapter);
+                else{
+                    Toast.makeText(Home_users.this, "No Restaurant in this location! ", Toast.LENGTH_SHORT).show();
+                }
             }
 
-            //if there is an error pulling data from the data base show messege
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Toast.makeText(Home_users.this, "cant show resturuonts", Toast.LENGTH_LONG).show();
 
             }
         });
-
-
     }
 
     //click functoin on personal settings button
@@ -111,8 +118,8 @@ public class Home_users extends AppCompatActivity implements View.OnClickListene
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+        text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
     }
 
     @Override
