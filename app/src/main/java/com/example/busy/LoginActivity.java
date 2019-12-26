d2eee16100c69ac69477809656745e51f40689eb
TalNoamX <talnoamb@gmail.com>
2019-12-26 10:05:06 +0200

    log in is now working
    got bug home users crush

--- app/src/main/java/com/example/busy/LoginActivity.java
+++ app/src/main/java/com/example/busy/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.busy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,17 +10,24 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.busy.restaurant.HOME_restaurant;
 import com.example.busy.users.Home_users;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth mAuth;
     private EditText email_ID, password_ID;
-    private Button BTN_login;
-    private FirebaseAuth.AuthStateListener mAuth_SL;
+    private DatabaseReference ref_users;
+    private String UID;
+    private String owner;
 
 
     @Override
@@ -40,56 +46,75 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         //Init class variables
+
+        findViewById(R.id.loginBotton).setOnClickListener(this);
         mAuth = FirebaseAuth.getInstance();
         email_ID = findViewById(R.id.EmailLog);
         password_ID = findViewById(R.id.passwordLog);
-        BTN_login = findViewById(R.id.loginBotton);
 
-//        mAuth_SL = new FirebaseAuth.AuthStateListener() {
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//                FirebaseUser mUser = mAuth.getCurrentUser();
-//                if(mUser != null){
-//                    Toast.makeText(LoginActivity.this,"Log In Succesfull",Toast.LENGTH_LONG).show();
-//                    Intent i = new Intent(LoginActivity.this,Home_users.class);
-//                    startActivity(i);
-//                }
-//            }
-//        };
-        BTN_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = email_ID.getText().toString();
-                String password = password_ID.getText().toString();
-                if (email.isEmpty()) {
-                    email_ID.setError("Missing Email");
-                    email_ID.requestFocus();
-                } else if (password.isEmpty()) {
-                    password_ID.setError("Missing Password");
-                    password_ID.requestFocus();
-                } else if (!(email.isEmpty() && password.isEmpty())) {
-                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (!task.isSuccessful()) {
-                                Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_LONG).show();
-                            } else {
-                                Toast.makeText(LoginActivity.this, "Log In Succesfull", Toast.LENGTH_LONG).show();
-                                Intent i_home = new Intent(LoginActivity.this, Home_users.class);
-                                startActivity(i_home);
-                            }
-                        }
-                    });
-                } else {
-                    Toast.makeText(LoginActivity.this, "Error, Try again", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+
     }
 
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        mAuth.addAuthStateListener(mAuth_SL);
-//    }
+    public void LOGIN() {
+        String email = email_ID.getText().toString();
+        String password = password_ID.getText().toString();
+        if (email.isEmpty()) {
+            email_ID.setError("Missing Email");
+            email_ID.requestFocus();
+
+        } else if (password.isEmpty()) {
+            password_ID.setError("Missing Password");
+            password_ID.requestFocus();
+
+        } else if (!(email.isEmpty() && password.isEmpty())) {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Log In Succesfull", Toast.LENGTH_LONG).show();
+
+                        ref_users = FirebaseDatabase.getInstance().getReference("Users"); //get reference to Users
+                        UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                        ref_users.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                owner = dataSnapshot.child(UID).child("owner").getValue().toString(); // get user id of the current user
+                                Intent i;
+                                if (owner.equals("no")) {
+                                    i = new Intent(LoginActivity.this, Home_users.class);
+                                    startActivity(i);
+                                } else {
+                                    i = new Intent(LoginActivity.this, HOME_restaurant.class);
+                                    startActivity(i);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(LoginActivity.this, "Error, Try again", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginBotton:
+                LOGIN();
+                break;
+        }
+    }
+
 }
