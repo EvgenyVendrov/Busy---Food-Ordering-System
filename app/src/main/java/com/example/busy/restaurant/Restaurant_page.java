2168f5bcdadd174c7f15313887607e658815cde9
TalNoamX <talnoamb@gmail.com>
2020-01-02 11:44:54 +0200

    fixing bugs

---
+++ app/src/main/java/com/example/busy/restaurant/Restaurant_page.java
@@ -0,0 +1,18 @@
+package com.example.busy.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.busy.R;
+
+public class Restaurant_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_page);
+
+
+    }
+}
