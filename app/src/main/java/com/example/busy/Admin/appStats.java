084206737174917e387fc3690a4110e7e0c8829f
Evgeny Vendrov <evgenyv94@gmail.com>
2020-01-09 13:30:06 +0200

    added more activities - for now all acts are included

---
+++ app/src/main/java/com/example/busy/Admin/appStats.java
@@ -0,0 +1,16 @@
+package com.example.busy.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.busy.R;
+
+public class appStats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_stats);
+    }
+}
--- app/src/main/java/com/example/busy/restaurant/OrderHistoryRest.java
+++ app/src/main/java/com/example/busy/restaurant/OrderHistoryRest.java
@@ -2,15 +2,26 @@ package com.example.busy.restaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.busy.R;
 
-public class OrderHistoryRest extends AppCompatActivity {
+public class OrderHistoryRest extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history_rest);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.stats_btn_rest:
+                Intent i = new Intent(OrderHistoryRest.this, RestStaticstics.class);
+                startActivity(i);
+        }
     }
 }
