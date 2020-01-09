4ce8ed37202c1b3f4b19dd590c711a1b7eed6d1a
TalNoamX <talnoamb@gmail.com>
2020-01-09 15:22:02 +0200

    add a feature for restaurant page to call the rest

--- app/src/main/java/com/example/busy/restaurant/Restaurant_page.java
+++ app/src/main/java/com/example/busy/restaurant/Restaurant_page.java
@@ -1,19 +1,19 @@
 package com.example.busy.restaurant;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.busy.R;
 import com.example.busy.restaurant.Rforms.Restaurant_Form;
 import com.example.busy.users.Make_Order;
-import com.example.busy.users.Uform.filter_form;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,28 +28,29 @@ public class Restaurant_page extends AppCompatActivity {
     private TextView rest_info;
     private Button menubtn;
     private String rest_uid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_page);
 
         Intent i = getIntent();
-        final String uid =(String) i.getSerializableExtra("rest_uid");
+        final String uid = (String) i.getSerializableExtra("rest_uid");
         menubtn = findViewById(R.id.menu_rest_page);
         rest_info = findViewById(R.id.restname_Rest_page);
         rest_database = FirebaseDatabase.getInstance().getReference("Restaurant");
         rest_database.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
-                    for (DataSnapshot snep : dataSnapshot.getChildren()){
-                        if (snep.child("uid").getValue().equals(uid)){
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snep : dataSnapshot.getChildren()) {
+                        if (snep.child("uid").getValue().equals(uid)) {
                             rest = snep.getValue(Restaurant_Form.class);
                             rest_uid = uid;
                         }
                     }
-                    String show ="Name: " + rest.getName() + "\n" + "Location: " + rest.getLocation() + "\n" + "phone: " + rest.getPhone()
-                            + "\n" + "kosher: " + rest.getKosher() + "\n" + rest.getType() + "\n" +"\n" + rest.getDescription();
+                    String show = "Name: " + rest.getName() + "\n" + "Location: " + rest.getLocation() + "\n" + "phone: " + rest.getPhone()
+                            + "\n" + "kosher: " + rest.getKosher() + "\n" + rest.getType() + "\n" + "\n" + rest.getDescription();
                     rest_info.setText(show);
 
                 }
@@ -65,8 +66,26 @@ public class Restaurant_page extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Restaurant_page.this, Make_Order.class);
-                i.putExtra("rest_uid",rest_uid);
+                i.putExtra("rest_uid", rest_uid);
                 startActivity(i);
+            }
+        });
+
+        rest_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri u = Uri.parse("tel:" + rest.getPhone());
+                Intent _i = new Intent(Intent.ACTION_DIAL, u);
+
+                try {
+                    // Launch the Phone app's dialer with a phone
+                    // number to dial a call.
+                    startActivity(_i);
+                } catch (SecurityException s) {
+                    // show() method display the toast with
+                    // exception message.
+                    Toast.makeText(Restaurant_page.this, s.getMessage(), Toast.LENGTH_LONG).show();
+                }
             }
         });
 
